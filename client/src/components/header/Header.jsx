ce84304e30027e4ea86ebd401695dd2180e00af9
Bekzod <bekzodisakov18@gmail.com>
2023-07-08 11:02:13 +0500

    client - feat:header responsiveness

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -16,15 +16,18 @@ import {
   ModalContent,
   ModalHeader,
   ModalOverlay,
+  Select,
   Text,
+  useColorMode,
   useColorModeValue,
   useDisclosure,
 } from "@chakra-ui/react";
 import Navigation from "./Navigations/Navigation";
-import { useAuth } from "../../provider/authProvider";
+import { useAuth } from "../../providers/authProvider";
 import { useUserSignOut } from "../../hooks/user";
 import SVG from "../SVG";
 import React from "react";
+import { useI18n } from "../../providers/i18nProvider";
 
 const Header = () => {
   const [searchText, setSearchText] = React.useState("");
@@ -33,12 +36,18 @@ const Header = () => {
 
   const { token, user, setToken } = useAuth();
 
+  const { languageCodes, language } = useI18n();
+
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   const handleSignOut = () => {
     onSignOut();
     setToken(null);
   };
+
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  const initialRef = React.useRef(null);
 
   return (
     <Box
@@ -51,74 +60,112 @@ const Header = () => {
       <Flex h={14} px={4} alignItems={"center"} justifyContent='space-between'>
         {token ? <Navigation /> : <div></div>}
 
-        <IconButton
-          icon={<SVG iconId={"search"} size={"20px"} />}
-          variant={"ghost"}
-          size={"sm"}
-          onClick={onOpen}
-          aria-label='Search database'
-          title={"Search"}
-          _hover={{ bg: "transparent" }}
-        />
+        <HStack alignItems='center' h='40%'>
+          <Select
+            ml={15}
+            size={"sm"}
+            display={{ base: "none", xs: "block" }}
+            variant={"filled"}
+            rounded={"md"}
+          >
+            {Object.keys(languageCodes).map((key) => (
+              <option key={key}>{languageCodes[key]}</option>
+            ))}
+          </Select>
 
-        <Modal isOpen={isOpen} onClose={onClose} scrollBehavior='inside'>
-          <ModalOverlay />
-          <ModalContent mx={3}>
-            <ModalHeader>Search items</ModalHeader>
-            <ModalCloseButton />
-            <ModalBody pb={5}>
-              <InputGroup>
-                <Input
-                  value={searchText}
-                  onChange={(e) => setSearchText(e.target.value)}
-                />
-                <InputRightElement>
-                  <IconButton
-                    onClick={onOpen}
-                    aria-label='Search database'
-                    size={"sm"}
-                    isLoading={false}
-                    isDisabled={searchText.length < 2}
-                    title={"Search"}
-                    icon={<SVG iconId={"search"} size={"20px"} />}
-                    colorScheme={"blue"}
-                  />
-                </InputRightElement>
-              </InputGroup>
-            </ModalBody>
-          </ModalContent>
-        </Modal>
+          <IconButton
+            onClick={toggleColorMode}
+            variant={"ghost"}
+            size={"sm"}
+            icon={
+              colorMode === "light" ? (
+                <SVG iconId={"moon"} />
+              ) : (
+                <SVG iconId={"sun"} size={"22px"} />
+              )
+            }
+          />
 
-        {token ? (
-          <HStack alignItems='center' h='40%'>
-            <HStack mr={3}>
-              <Avatar name={user.name} src='' size='xs' />
-              <Text whiteSpace='nowrap' display={{ base: "none", md: "block" }}>
-                {user.name}
-              </Text>
-            </HStack>
-            <Divider
-              orientation='vertical'
-              variant='solid'
-              borderColor='gray.400'
-            />
-            <Button
-              isLoading={loading}
-              loadingText={"Sign out"}
-              variant={"ghost"}
-              colorScheme={"red"}
-              size={"sm"}
-              onClick={handleSignOut}
-            >
-              Sign out
+          <IconButton
+            icon={<SVG iconId={"search"} />}
+            variant={"ghost"}
+            size={"sm"}
+            onClick={onOpen}
+            aria-label='Search database'
+            title={"Search"}
+            _hover={{ bg: "transparent" }}
+            mr={2}
+          />
+          {token ? (
+            <>
+              <HStack>
+                <Avatar name={user.name} src='' size='xs' />
+                <Text
+                  whiteSpace='nowrap'
+                  display={{ base: "none", md: "block" }}
+                >
+                  {user.name}
+                </Text>
+              </HStack>
+              <Divider
+                orientation='vertical'
+                variant='solid'
+                borderColor='gray.400'
+                mx={2}
+              />
+              <Button
+                isLoading={loading}
+                loadingText={"Sign out"}
+                variant={"ghost"}
+                colorScheme={"red"}
+                size={"sm"}
+                onClick={handleSignOut}
+                px={{base: "2px", xs: "12px"}}
+              >
+                Sign out
+              </Button>
+            </>
+          ) : (
+            <Button variant={"link"} colorScheme={"blue"} size={"sm"}>
+              <Link to='/signin'>Sign in</Link>
             </Button>
-          </HStack>
-        ) : (
-          <Button variant={"link"} colorScheme={"blue"} size={"sm"}>
-            <Link to='/signin'>Sign in</Link>
-          </Button>
-        )}
+          )}
+        </HStack>
       </Flex>
+
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+        scrollBehavior='inside'
+        initialFocusRef={initialRef}
+      >
+        <ModalOverlay />
+        <ModalContent mx={3}>
+          <ModalHeader>Search items</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={5}>
+            <InputGroup>
+              <Input
+                ref={initialRef}
+                value={searchText}
+                onChange={(e) => setSearchText(e.target.value)}
+              />
+              <InputRightElement>
+                <IconButton
+                  onClick={onOpen}
+                  aria-label='Search database'
+                  size={"sm"}
+                  isLoading={false}
+                  isDisabled={searchText.length < 2}
+                  title={"Search"}
+                  icon={<SVG iconId={"search"} size={"20px"} />}
+                  colorScheme={"blue"}
+                />
+              </InputRightElement>
+            </InputGroup>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 };
