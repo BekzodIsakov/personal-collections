1fc9d5ea6d9f83aef7a95be14bda015ef731cc03
Bekzod <bekzodisakov18@gmail.com>
2023-07-08 11:00:36 +0500

    client - add: provide default size of 18px for svg size

--- client/src/components/SVG.jsx
+++ client/src/components/SVG.jsx
@@ -1,6 +1,6 @@
 import sprite from "../assets/sprite.svg";
 
-const SVG = ({ iconId, size, ...props }) => {
+const SVG = ({ iconId, size = 18, ...props }) => {
   return (
     <svg width={size} height={size} {...props}>
       <use xlinkHref={`${sprite}#${iconId}`} />
