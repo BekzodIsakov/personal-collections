254e767ba079c759cdd534c8a4c30ccb17baffc1
Bekzod <bekzodisakov18@gmail.com>
2023-07-10 18:54:16 +0500

    client - edit: default value for redirectPath prop

--- client/src/routes/ProtectedRoute.jsx
+++ client/src/routes/ProtectedRoute.jsx
@@ -2,7 +2,7 @@ import { Navigate, Outlet } from "react-router-dom";
 // import Header from "../components/header/Header";
 import { useAuth } from "../providers/authProvider";
 
-export const ProtectedRoute = ({ redirectPath = "/login" }) => {
+export const ProtectedRoute = ({ redirectPath = "/signin" }) => {
   const { token } = useAuth();
   console.log({ token });
   if (!token) {
