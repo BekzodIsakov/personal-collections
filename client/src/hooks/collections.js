304724959f61e9c012b3ea5cad8f6f6da7678fdb
Bekzod <bekzodisakov18@gmail.com>
2023-07-13 10:47:50 +0500

    client - add: useFetchCollectionItems hook

--- client/src/hooks/collections.js
+++ client/src/hooks/collections.js
@@ -114,3 +114,33 @@ export const useDeleteCollectionImage = () => {
     deleteCollectionImage,
   };
 };
+
+export const useFetchCollectionItems = () => {
+  const [loading, setLoading] = React.useState(false);
+  const [errorMessage, setErrorMessage] = React.useState("");
+  const [collection, setCollection] = React.useState(null);
+
+  async function fetchCollection(id, page, limit) {
+    try {
+      setLoading(true);
+      const result = await axios.get(
+        `${
+          import.meta.env.VITE_DEV_URL
+        }/collections/${id}/items/?page=${page}&limit=${limit}`
+      );
+      setCollection(result.data);
+    } catch (error) {
+      setErrorMessage(error.response.data.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return {
+    loading,
+    errorMessage,
+    collection,
+    setCollection,
+    fetchCollection,
+  };
+};
