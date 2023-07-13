d5dbda6eba65bc33263de11c010c93d1cd0cc6c2
Bekzod <bekzodisakov18@gmail.com>
2023-07-13 10:43:03 +0500

    client - add: ReactTable component

---
+++ client/src/components/ReactTable.jsx
@@ -0,0 +1,86 @@
+import React from "react";
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  chakra,
+  TableContainer,
+} from "@chakra-ui/react";
+import { TriangleDownIcon, TriangleUpIcon } from "@chakra-ui/icons";
+import {
+  useReactTable,
+  flexRender,
+  getCoreRowModel,
+  // ColumnDef,
+  // SortingState,
+  getSortedRowModel,
+} from "@tanstack/react-table";
+
+const ReactTable = ({ data, columns, onRowSelect, selectedRow, getRowId }) => {
+  const [sorting, setSorting] = React.useState([]);
+  const table = useReactTable({
+    columns,
+    data,
+    getRowId,
+    getCoreRowModel: getCoreRowModel(),
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    state: {
+      sorting,
+      rowSelection: selectedRow,
+    },
+    enableRowSelection: true,
+    enableMultiRowSelection: false,
+    onRowSelectionChange: onRowSelect,
+  });
+
+  return (
+    <TableContainer>
+      <Table>
+        <Thead>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <Tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <Th
+                  key={header.id}
+                  onClick={header.column.getToggleSortingHandler()}
+                >
+                  {flexRender(
+                    header.column.columnDef.header,
+                    header.getContext()
+                  )}
+
+                  <chakra.span pl='4'>
+                    {header.column.getIsSorted() ? (
+                      header.column.getIsSorted() === "desc" ? (
+                        <TriangleDownIcon aria-label='sorted descending' />
+                      ) : (
+                        <TriangleUpIcon aria-label='sorted ascending' />
+                      )
+                    ) : null}
+                  </chakra.span>
+                </Th>
+              ))}
+            </Tr>
+          ))}
+        </Thead>
+        <Tbody>
+          {table.getRowModel().rows.map((row) => (
+            <Tr key={row.id}>
+              {row.getVisibleCells().map((cell) => (
+                <Td key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </Td>
+              ))}
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default ReactTable;
