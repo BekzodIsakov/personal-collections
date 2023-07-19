2bf9db3ff721d1482e3989c0f571a4aa2db4b36a
Bekzod <bekzodisakov18@gmail.com>
2023-07-19 09:30:38 +0500

    app - add: fetchUser controller

--- app/src/controllers/users.js
+++ app/src/controllers/users.js
@@ -1,3 +1,5 @@
+const User = require("../models/userModel");
+
 const signOutUser = async (req, res) => {
   try {
     req.user.tokens = req.user.tokens.filter((token) => token != req.token);
@@ -10,6 +12,17 @@ const signOutUser = async (req, res) => {
   }
 };
 
+const fetchUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) return res.status(404).send({ message: "User not found!" });
+    res.send(user);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+};
+
 module.exports = {
   signOutUser,
+  fetchUser,
 };
