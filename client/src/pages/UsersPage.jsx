efd044b38369ce383a60855833418490e0319893
Bekzod <bekzodisakov18@gmail.com>
2023-07-19 12:22:08 +0500

    client - add: Spinner component

--- client/src/pages/UsersPage.jsx
+++ client/src/pages/UsersPage.jsx
@@ -1,11 +1,16 @@
-import { Box, Heading, ListItem, UnorderedList, Link } from "@chakra-ui/react";
+import {
+  Box,
+  Heading,
+  ListItem,
+  Spinner,
+  UnorderedList,
+} from "@chakra-ui/react";
 import React from "react";
 import { useFetchUsers } from "../hooks/user";
-import { Link as RouterLink } from "react-router-dom";
 import CustomLink from "../components/CustomLink";
 
 const UsersPage = () => {
-  const { loading, errorMessage, fetchUsers, users } = useFetchUsers();
+  const { loading, fetchUsers, users } = useFetchUsers();
   console.log({ users });
 
   React.useEffect(() => {
@@ -18,14 +23,15 @@ const UsersPage = () => {
         Users
       </Heading>
       <UnorderedList>
-        {users.map((user) => (
-          <ListItem key={user._id}>
-            {/* <Link as={<RouterLink />} to={`/${user._id}`}>
-              {user.name}
-            </Link> */}
-            <CustomLink to={`${user._id}`}>{user.name}</CustomLink>
-          </ListItem>
-        ))}
+        {loading ? (
+          <Spinner />
+        ) : (
+          users.map((user) => (
+            <ListItem key={user._id}>
+              <CustomLink to={`${user._id}`}>{user.name}</CustomLink>
+            </ListItem>
+          ))
+        )}
       </UnorderedList>
     </Box>
   );
