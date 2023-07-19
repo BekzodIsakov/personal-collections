0af67763fb095c300e5f4819b274b363e4b97d0c
Bekzod <bekzodisakov18@gmail.com>
2023-07-19 09:47:03 +0500

    client - edit: edit to custom hook

--- client/src/components/header/Navigations/navData.js
+++ client/src/components/header/Navigations/navData.js
@@ -1,9 +1,52 @@
-export default [
-  {
-    label: "My page",
-    to: "/my-page",
-  },
-];
+import React from "react";
+import { useAuth } from "../../../providers/authProvider";
+
+export const useNavData = () => {
+  const { token, user } = useAuth();
+
+  const authenticatedNavItems = React.useMemo(
+    () => [{ label: "My page", to: "/me" }],
+    []
+  );
+
+  const adminNavItems = React.useMemo(
+    () => [
+      { label: "Admin page", to: `admin/${user.id}` },
+      { label: "Users page", to: "users" },
+    ],
+    [user]
+  );
+
+  const [navData, setNavData] = React.useState([]);
+
+  React.useEffect(() => {
+    if (token) {
+      const _navData = [...authenticatedNavItems];
+
+      if (user.isAdmin) {
+        _navData.push(...adminNavItems);
+      }
+
+      setNavData(_navData);
+    }
+  }, [user]);
+
+  return {
+    navData,
+    setNavData,
+  };
+};
+
+// export default [
+//   {
+//     label: "My page",
+//     to: "/my-page",
+//   },
+//   ( ? {
+//     label: "My page",
+//     to: "/my-page",
+//   })
+// ];
 
 // Sample data
 // [
