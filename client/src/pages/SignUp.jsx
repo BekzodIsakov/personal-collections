c9700181828124d5cd011553ec3f143254ff6031
Bekzod <bekzodisakov18@gmail.com>
2023-08-01 20:40:12 +0500

    client - fix: Signin/Signup card

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -65,7 +65,13 @@ const SignUp = () => {
       justify='center'
       bg={useColorModeValue("gray.50", "gray.800")}
     >
-      <Stack spacing='8' mx='auto' maxW='lg' py='12' px='6'>
+      <Stack
+        spacing='8'
+        mx='auto'
+        width={{ base: "100%", sm: "430px" }}
+        py='12'
+        px='6'
+      >
         <Stack align='center'>
           <Heading fontSize='4xl'>{t("auth.headings.signUp")}</Heading>
         </Stack>
