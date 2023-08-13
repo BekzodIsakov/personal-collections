b86cc5635042d5a72af89575b4ba3d6f64d2fded
Bekzod <bekzodisakov18@gmail.com>
2023-08-13 15:19:11 +0500

    store users and rooms

---
+++ app/src/utils/users.js
@@ -0,0 +1,26 @@
+const users = [];
+
+function getUser(id) {
+  return users.find((user) => user.id === id);
+}
+
+function addUser({ id, userId, roomId }) {
+  const user = { id, userId, roomId };
+  users.push(user);
+  return user;
+}
+
+function removeUser(id) {
+  console.log("remove user");
+  const userIndex = users.findIndex((user) => user.id === id);
+  if (userIndex > -1) {
+    return users.splice(userIndex, 1)[0];
+  }
+}
+
+module.exports = {
+  users,
+  getUser,
+  addUser,
+  removeUser,
+};
