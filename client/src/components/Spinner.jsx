a8619db4d825f835ff2f53f814248c86fefb1b0c
Bekzod <bekzodisakov18@gmail.com>
2023-10-12 11:03:08 +0500

    fix Spinner component

--- client/src/components/Spinner.jsx
+++ client/src/components/Spinner.jsx
@@ -1,4 +1,4 @@
-import { ChakraSpinner } from "@chakra-ui/react";
+import { Spinner as ChakraSpinner } from "@chakra-ui/react";
 
 const Spinner = ({ color = "blue.500", ...otherProps }) => {
   return <ChakraSpinner color={color} {...otherProps} />;
