69584074adf5e70ba3b821312e3a868262c5a163
Bekzod <bekzodisakov18@gmail.com>
2023-10-13 10:43:57 +0500

    remove extra logs

--- client/src/hooks/items.js
+++ client/src/hooks/items.js
@@ -99,9 +99,6 @@ export const useCreateItem = () => {
   const [errorMessage, setErrorMessage] = React.useState("");
   const [createdItem, setCreatedItem] = React.useState(null);
 
-  console.log("useCreateItem");
-  console.log({ loading });
-
   async function createItem(item) {
     try {
       setLoading(true);
