50ce718bfb7fac9cc95a0852d42f129192b5244d
Bekzod <bekzodisakov18@gmail.com>
2024-01-30 19:16:50 +0500

    edit Spinner size

--- client/src/components/Spinner.jsx
+++ client/src/components/Spinner.jsx
@@ -1,7 +1,7 @@
 import { Spinner as ChakraSpinner } from "@chakra-ui/react";
 
 const Spinner = ({ color = "blue.500", ...otherProps }) => {
-  return <ChakraSpinner color={color} {...otherProps} />;
+  return <ChakraSpinner size='sm' color={color} {...otherProps} />;
 };
 
 export default Spinner;
